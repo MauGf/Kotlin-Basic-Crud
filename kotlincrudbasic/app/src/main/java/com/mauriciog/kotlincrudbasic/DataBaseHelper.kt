64be70de4abb281401db352ec61c5fdb771bf366
MauGf <maugarciaflores@gmail.com>
2018-05-08 23:46:14 -0600

    Added feature "Update"

--- kotlincrudbasic/app/src/main/java/com/mauriciog/kotlincrudbasic/DataBaseHelper.kt
+++ kotlincrudbasic/app/src/main/java/com/mauriciog/kotlincrudbasic/DataBaseHelper.kt
@@ -6,10 +6,10 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1 ){
+class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1 ) {
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL ("CREATE TABLE $TABLE_NAME(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT" +
-        ",EMAIL TEXT, MOBILE INTEGER)")
+        db?.execSQL("CREATE TABLE $TABLE_NAME(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT" +
+                ",EMAIL TEXT, MOBILE INTEGER)")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
@@ -17,13 +17,14 @@ class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
     }
 
     //Recuperar Datos
-    val allData : Cursor
-    get() {val db = this.writableDatabase
-        return db.rawQuery("Select * from ${TABLE_NAME}", null)
-    }
+    val allData: Cursor
+        get() {
+            val db = this.writableDatabase
+            return db.rawQuery("Select * from ${TABLE_NAME}", null)
+        }
 
-
-    fun insertData(name : String, email : String, mobile : String): Boolean{
+    //Isert data
+    fun insertData(name: String, email: String, mobile: String): Boolean {
         val db = this.writableDatabase
         val cv = ContentValues()
         cv.put(COL_2, name)
@@ -32,16 +33,32 @@ class DataBaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME
 
         val result = db.insert(TABLE_NAME, null, cv)
         db.close()
-        return if (result.equals(-1)){
+        return if (result.equals(-1)) {
             false
-        }else{
+        } else {
             true
         }
     }
 
-    companion  object{
+    //Update data
+    fun updateData(id: String, name: String, email: String, mobile: String): Boolean {
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(COL_2, name)
+        cv.put(COL_3, email)
+        cv.put(COL_4, mobile)
+
+        val result = db.update(TABLE_NAME, cv, "ID = ?", arrayOf(id))
+        return if (result > 0) {
+            true
+        } else {
+            false
+        }
+    }
+
+    companion object {
         val DATABASE_NAME = "Student.db"
-        val TABLE_NAME    = "Student_table"
+        val TABLE_NAME = "Student_table"
 
 
         val COL_1 = "ID"
--- kotlincrudbasic/app/src/main/java/com/mauriciog/kotlincrudbasic/MainActivity.kt
+++ kotlincrudbasic/app/src/main/java/com/mauriciog/kotlincrudbasic/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     internal lateinit var  edit1 : EditText
     internal lateinit var  edit2 : EditText
     internal lateinit var  edit3 : EditText
+    internal lateinit var  edit4 : EditText
+    internal lateinit var  btnUpdate : Button
     internal lateinit var  btn   : Button
     internal lateinit var  btn2  : Button
     internal lateinit var  txt   : TextView
@@ -25,29 +27,32 @@ class MainActivity : AppCompatActivity() {
 
         myDb = DataBaseHelper(this)
 
-        edit1 = findViewById<View>(R.id.id_name) as EditText
-        edit2 = findViewById<View>(R.id.id_name) as EditText
-        edit3 = findViewById<View>(R.id.id_name) as EditText
-        btn   = findViewById<View>(R.id.insert)  as Button
-        btn2  = findViewById<View>(R.id.id_read) as Button
-        txt   = findViewById<View>(R.id.txt)     as TextView
+        edit1 = findViewById<View>(R.id.id_name)   as EditText
+        edit2 = findViewById<View>(R.id.id_email)  as EditText
+        edit3 = findViewById<View>(R.id.id_mobile) as EditText
+        edit4 = findViewById<View>(R.id.id_txt)    as EditText
+        btnUpdate   = findViewById<View>(R.id.id_update) as Button
+        btn   = findViewById<View>(R.id.insert)    as Button
+        btn2  = findViewById<View>(R.id.id_read)   as Button
+        txt   = findViewById<View>(R.id.txt)       as TextView
 
         btn2.setOnClickListener {
             ReadMe()
         }
-
         btn.setOnClickListener{(ClicKMe())}
+        btnUpdate.setOnClickListener { UpdateMe() }
     }
 
+    // Leer Datos
     private fun ReadMe() {
         val res    = myDb.allData
         val buffer = StringBuffer()
         if(res != null && res.count>0){
             while (res.moveToNext()){
-                buffer.append("ID:" + res.getString(0) + "\n")
-                buffer.append("NAME:" + res.getString(1) + "\n")
-                buffer.append("EMAIL:" + res.getString(2) + "\n")
-                buffer.append("MOBILE:" + res.getString(3) + "\n")
+                buffer.append("ID: " + res.getString(0) + "\n")
+                buffer.append("NAME: " + res.getString(1) + "\n")
+                buffer.append("EMAIL: " + res.getString(2) + "\n")
+                buffer.append("MOBILE: " + res.getString(3) + "\n\n")
             }
             txt.text = buffer.toString()
             Toast.makeText(this, "Data Retrieved Successfully", Toast.LENGTH_SHORT).show()
@@ -57,11 +62,11 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-
+    //inset data
     private fun ClicKMe() {
 
-        val name = edit1.text.toString()
-        val email = edit2.text.toString()
+        val name   = edit1.text.toString()
+        val email  = edit2.text.toString()
         val mobile = edit3.text.toString()
 
         val result = myDb.insertData(name, email, mobile)
@@ -71,4 +76,17 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Data insertion Failed", Toast.LENGTH_SHORT).show()
     }
 
+    //Update data
+    private fun UpdateMe(){
+        val name   = edit1.text.toString()
+        val email  = edit2.text.toString()
+        val mobile = edit3.text.toString()
+        val id     = edit4.text.toString()
+
+        val result = myDb.updateData(id, name, email, mobile)
+        if (result == true)
+            Toast.makeText(this, "Data Updated Successfully", Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(this, "Data updation Failed", Toast.LENGTH_SHORT).show()
+    }
 }
